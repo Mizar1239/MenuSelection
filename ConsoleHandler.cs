a8decd076bff1b8ea9ae4fd0ef2cb9581dc4a75a
Mizar1239 <paolobale1239@yahoo.it>
2024-04-16 03:40:32 +0200

    Generic type implementation: the displayer now accepts eny kind of list

--- ConsoleHandler.cs
+++ ConsoleHandler.cs
@@ -1,58 +1,52 @@
-public class ConsoleHandler
+using Microsoft.Win32.SafeHandles;
+
+public interface IDisplayable
+{
+	public string displayString();
+}
+
+public class ConsoleHandler<T>
 {
 	private readonly ConsoleColor _defaultBackgroundColor, _defaultForegroundColor;
-	string[] options;
+	private readonly T[] _options;
+	private readonly Func<T, string> _toString;
 
-	public ConsoleHandler(List<string> options)
+	public ConsoleHandler(T[] options, Func<T, string> stringingOptions)
 	{
-		this.options = options.ToArray();
+		this._options = options;
+		this._toString = stringingOptions;
 
 		this._defaultBackgroundColor = Console.BackgroundColor;
 		this._defaultForegroundColor = Console.ForegroundColor;
 	}
+	
+	public ConsoleHandler(List<T> options, Func<T, string> stringingOptions) 
+		: this(options.ToArray(), stringingOptions)
+	{ }
 
-	public ConsoleHandler(string[] options)
-	{
-		this.options = options;
-
-		this._defaultBackgroundColor = Console.BackgroundColor;
-		this._defaultForegroundColor = Console.ForegroundColor;
-	}
-
-	public ConsoleHandler(List<string> options, ConsoleColor backgroundColor, ConsoleColor foregroundColor)
-	{
-		this.options = options.ToArray();
-
-		this._defaultBackgroundColor = backgroundColor;
-		this._defaultForegroundColor = foregroundColor;
-	}
-
-	public string DisplaySelectionMenu()
+	public T DisplaySelectionMenu()
 	{
 		int previousLineIndex = -1, selectedLineIndex = 0;
 		ConsoleKey pressedKey = ConsoleKey.None;
 
 		bool sortingMode = false;
 
+		Console.WriteLine(SELECTION_TEXT);
+
 		do 
 		{
 			if (previousLineIndex != selectedLineIndex)
 			{
-				this._updateMenu(selectedLineIndex, this.options);
+				this._updateMenu(selectedLineIndex);
 				previousLineIndex = selectedLineIndex;
 			}
 
 			pressedKey = Console.ReadKey().Key;
 
-			if (pressedKey == ConsoleKey.DownArrow && selectedLineIndex+1 < this.options.Length)
+			if (pressedKey == ConsoleKey.DownArrow && selectedLineIndex+1 < this._options.Length)
 			{
 				if (sortingMode)
-				{
-					// var tmp = this.options[selectedLineIndex+1];
-					// this.options[selectedLineIndex+1] = this.options[selectedLineIndex];
-					// this.options[selectedLineIndex] = tmp;
 					this._swap(selectedLineIndex+1, selectedLineIndex);
-				}
 
 				selectedLineIndex++;
 			}
@@ -68,39 +62,42 @@ public class ConsoleHandler
 		}
 		while (pressedKey != ConsoleKey.Enter);
 
-		Console.WriteLine($"Option chosen: {this.options[selectedLineIndex]}");
-
-		return this.options[selectedLineIndex];
+		return this._options[selectedLineIndex];
 	}
 
-	private void _updateMenu(int index, string[] cities)
+	private void _updateMenu(int index)
 	{
 		Console.Clear();
+		Console.WriteLine("\x1b[3J");
 
-		foreach (var city in cities)
+		Console.WriteLine(SELECTION_TEXT);
+
+		foreach (var item in this._options)
 		{
-			bool isSelected = city == cities[index];
+			bool isSelected = item != null && item.Equals(this._options[index]);
 
 			if (isSelected)
-				this._drawSelectedLine(city);
+				this._drawSelectedLine(item);
 			else
-				Console.WriteLine($"  {city}");
+				Console.WriteLine($"  {this._toString(item)}");
 		}
 	}
 
-	private void _drawSelectedLine(string city)
+	private void _drawSelectedLine(T item)
 	{
 		Console.BackgroundColor = ConsoleColor.White;
 		Console.ForegroundColor = ConsoleColor.Black;
-		Console.WriteLine($"> {city}");
+		Console.WriteLine($"> {this._toString(item)}");
 		Console.BackgroundColor = this._defaultBackgroundColor;
 		Console.ForegroundColor = this._defaultForegroundColor;		
 	}
 
 	private void _swap(int index1, int index2)
 	{
-		string tmp = this.options[index1];
-		this.options[index1] = this.options[index2];
-		this.options[index2] = tmp;
+		T tmp = this._options[index1];
+		this._options[index1] = this._options[index2];
+		this._options[index2] = tmp;
 	}
+
+	private const string SELECTION_TEXT = "Use arrows to select an object in the list\nPress Space to select an object and the arrows to change its place\nPress Enter to finish\n";
 }
--- Program.cs
+++ Program.cs
@@ -6,13 +6,21 @@
 	"Firenze"
 };
 
-ConsoleHandler consHand = new ConsoleHandler(cities);
+Person[] people = {
+	new Person() { Name = "Paolo", Age = 23},
+	new Person() { Name = "Francesco", Age = 24},
+	new Person() { Name = "Gabriele", Age = 23},
+	new Person() { Name = "Mattia", Age = 26},
+	new Person() { Name = "Davide", Age = 28}
+};
 
-string selected = consHand.DisplaySelectionMenu();
+ConsoleHandler<Person> consHand = new ConsoleHandler<Person>(people, (p) => p.Name);
 
-foreach (var item in cities)
+Person selected = consHand.DisplaySelectionMenu();
+
+foreach (var item in people)
 {
 	Console.WriteLine(item);
 }
 
-Console.WriteLine($"Option chosen: {selected}");
+Console.WriteLine($"\nOption chosen: {selected}");
